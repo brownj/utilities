cd319dec30504b61b9f1d1ad11a66695006e775b
Josh Brown <josh@brownj.org>
2021-01-02 21:49:04 -0600

    add more ways to disable files from executing
    Signed-off-by: Josh Brown <josh@brownj.org>

--- run-parts/run-parts.py
+++ run-parts/run-parts.py
@@ -9,6 +9,7 @@ def excludedfilenames():
 
 def main():
 	import sys, os, subprocess
+	from itertools import islice
 
 	if (len(sys.argv) < 2):
 		print("Usage: run-parts.py [-n/--dry-run/--test] folder")
@@ -34,26 +35,41 @@ def main():
 	
 	for file in filesarr:
 		absfile = os.path.join(folder,file)
+		skip = ""
 
 		if file in excludedfiles:
 			# skip things in excluded file list
-			continue
+			skip = "excluded"
 
-		if not os.path.isfile(absfile):
+		elif not os.path.isfile(absfile):
 			# skip things that aren't files (i.e. folders)
-			continue
+			skip = "not-file"
 
-		if not os.access(absfile, os.X_OK):
+		elif not os.access(absfile, os.X_OK):
 			# skip things not executable
-			continue
+			skip = "not-exectuable"
+		
+		elif ("_disabled" in absfile):
+			# skip if _disabled in the filename
+			skip = "disabled via filename"
 
-		if testmode:
-			print("would run: " + absfile)
 		else:
-			print("running: " + absfile)
-			subprocess.call([
-				absfile
-			])
+			with open(absfile) as fin:
+				for line in islice(fin, 1, 2):
+					if ("#disabled" in line):
+						skip = "disabled via line 2"
+		
+		if testmode:
+			if (len(skip) == 0):
+				print("would run:  " + absfile)
+			else:
+				print("would skip: " + absfile + f" [reason: {skip}]")
+		else:
+			if (len(skip) == 0):
+				print("running: " + absfile)
+				subprocess.call( [ absfile ] )
+			else:
+				print("skipped: " + absfile + f" [reason: {skip}]")
 
 	sys.exit(0)
 
